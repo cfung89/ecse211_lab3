f3e1d87a8d266609815ed6a334eaf95e2b65009a
Flavie Qin <qinflavie@gmail.com>
2025-10-15 20:52:57 -0400

    fix: another version of flute to see if reduce choppiness

--- project/flute.py
+++ project/flute.py
@@ -7,69 +7,51 @@ import time
 DELAY_US = 0.01 # delay time between measurements
 
 # initialize 4 notes we will use
-noteA = Sound(duration=1, pitch="A4", volume=100)
-noteB = Sound(duration=1, pitch="B4", volume=100)
-noteC = Sound(duration=1, pitch="C5", volume=100)
-noteD = Sound(duration=1, pitch="D5", volume=100)
+noteA = Sound(duration=1, pitch="A4", volume=80)
+noteB = Sound(duration=1, pitch="B4", volume=80)
+noteC = Sound(duration=1, pitch="C5", volume=80)
+noteD = Sound(duration=1, pitch="D5", volume=80)
 
-def play_A():
-    """Plays the note A"""
-    if not noteA.is_playing():
-        noteA.play() # no wait done so will play until gets stopped
-    noteB.stop()
-    noteC.stop()
-    noteD.stop()
-
-def play_B():
-    """Plays the note B"""
-    if not noteB.is_playing():
-        noteB.play() # no wait done so will play until gets stopped
-    noteA.stop()
-    noteC.stop()
-    noteD.stop()
-
-def play_C():
-    """Plays the note C"""
-    if not noteC.is_playing():
-        noteC.play() # no wait done so will play until gets stopped
-    noteA.stop()
-    noteB.stop()
-    noteD.stop()
-
-def play_D():
-    """Plays the note D"""
-    if not noteD.is_playing():
-        noteD.play() # no wait done so will play until gets stopped
-    noteA.stop()
-    noteB.stop()
-    noteC.stop()
-
-def play_none():
-    """Plays no notes"""
-    noteA.stop()
-    noteB.stop()
-    noteC.stop()
-    noteD.stop()
+# store notes in dictionary for easier access
+NOTES = {
+    "A": noteA,
+    "B": noteB,
+    "C": noteC,
+    "D": noteD
+}
 
 def run_flute_subsystem(ultra: EV3UltrasonicSensor):
+    current = None # store the current note to play
+    next = None # stores the next note to play
+
     while True:
         distance = ultra.get_cm()
         if distance is not None:
             if 5 <= distance < 15:
                 # play an A
-                play_A()
+                next = "A"
             elif 15 <= distance < 25:
                 # play a B
-                play_B()
+                next = "B"
             elif 25 <= distance < 35: 
                 # play a C
-                play_C()
+                next = "C"
             elif 35 <= distance < 45:
                 # play a D
-                play_D()
+                next = "D"
             else:
                 # play nothing
-                play_none()
+                next = None
+        if next != current: 
+            # play new note if it changed
+            if current:
+                # stop old note
+                NOTES[current].stop()
+            if next:
+                # start playing new note with repeat and interval silence 0
+                NOTES[next].repeat_sound(100)
+
+            current = next
         time.sleep(DELAY_US)
 
 if __name__ == "__main__":
